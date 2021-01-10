d2389caa80e6d283192cf8545b0007a82b9607f9
Linuxinet <55936553+Linuxinet@users.noreply.github.com>
2021-01-10 08:17:42 +0530

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -1,67 +1,37 @@
 #! /bin/bash
+echo "Enter the user name: "  read user_name
+echo "Enter the password(4-6 character) : "  read pass 
 
 printf "Installing RDP Be Patience... " >&2
-
 {
-
-sudo useradd -m ALOK
-
-sudo adduser ALOK sudo
-
-echo 'ALOK:8426' | sudo chpasswd
-
+sudo useradd -m $user_name
+sudo adduser $user_name sudo
+echo '$user_name:$pass' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
-
 sudo apt-get update
-
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
-
 sudo apt install --assume-yes --fix-broken
-
 sudo DEBIAN_FRONTEND=noninteractive \
-
 apt install --assume-yes xfce4 desktop-base
-
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
-
 sudo apt install --assume-yes xscreensaver
-
 sudo systemctl disable lightdm.service
-
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-
 sudo dpkg --install google-chrome-stable_current_amd64.deb
-
 sudo apt install --assume-yes --fix-broken
-
 sudo apt install nautilus nano -y 
-
-sudo adduser ALOK chrome-remote-desktop
-
+sudo adduser $user_name chrome-remote-desktop
 } &> /dev/null &&
-
 printf "\nSetup Complete " >&2 ||
-
 printf "\nError Occured " >&2
-
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
-
-
-
-su - ALOK -c """DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AY0e-g6tGrBIqsVxKNyVm3eTChi_sueB0Ih2xej7PahHtJ_z2L3keeEN_8XkZOYyTWIGCg" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)"""
-
+read -p "Paste Here: " CRP
+su - $user_name -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
-
 if sudo apt-get upgrade &> /dev/null
-
 then
-
     printf "\n\nUpgrade Completed " >&2
-
 else
-
     printf "\n\nError Occured " >&2
-
 fi
