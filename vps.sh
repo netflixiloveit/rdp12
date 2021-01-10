977cce86f8683999888512fa6f7e7daba295465c
Linuxinet <55936553+Linuxinet@users.noreply.github.com>
2021-01-10 18:47:07 +0530

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
-read -p "Enter the user name: " user_name
 
+echo " User successfully created :- Ubuntu "
 echo " Password successfully created :- 1234 "
 
 
 printf "Installing RDP Be Patience... " >&2
 {
-sudo useradd -m $user_name
-sudo adduser $user_name sudo
-echo '$user_name:1234' | sudo chpasswd
+sudo useradd -m Ubuntu
+sudo adduser Ubuntu sudo
+echo 'Ubuntu:1234' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -23,13 +23,13 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo adduser $user_name chrome-remote-desktop
+sudo adduser Ubuntu chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - $user_name -c """$CRP"""
+su - Ubuntu -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
